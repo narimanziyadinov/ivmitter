672833f5e8e4e4b69127b46da4def35aa1305adc
narimanziyadinov <Viskiicui4ugeneva>
2020-01-26 22:42:11 +0300

    Redisign and add model  classes

--- app/src/main/java/narimanz/dev/ivmitter/UserActivityInfo.java
+++ app/src/main/java/narimanz/dev/ivmitter/UserActivityInfo.java
@@ -3,12 +3,69 @@ package narimanz.dev.ivmitter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import narimanz.dev.ivmitter.pojo.User;
 
 public class UserActivityInfo extends AppCompatActivity {
+
+    private ImageView userImageView;
+    private TextView nameTextView;
+    private TextView nickTextView;
+    private TextView descriptionTextView;
+    private TextView locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        userImageView = findViewById(R.id.user_image_view);
+        nameTextView = findViewById(R.id.user_name_text_view);
+        nickTextView = findViewById(R.id.user_nick_text_view);
+        descriptionTextView = findViewById(R.id.user_description_text_view);
+        locationTextView = findViewById(R.id.user_location_text_view);
+        followingCountTextView = findViewById(R.id.following_count_text_view);
+        followersCountTextView = findViewById(R.id.followers_count_text_view);
+
+        loadUserInfo();
+
+    }
+
+    private void loadUserInfo() {
+        User user = getUser();
+        displayUserInfo(user);
+    }
+
+    private void displayUserInfo(User user) {
+        Picasso.get().load(user.getImageUrl()).into(userImageView);
+        nameTextView.setText(user.getName());
+        nickTextView.setText(user.getNick());
+        descriptionTextView.setText(user.getDescription());
+        locationTextView.setText(user.getLocation());
+
+        String followingCount = String.valueOf(user.getFollowingCount());
+        followingCountTextView.setText(followingCount);
+
+        String followersCount = String.valueOf(user.getFollowersCount());
+        followersCountTextView.setText(followersCount);
+    }
+
+    private User getUser() {
+        return new User(
+                1L,
+                "http://i.imgur.com/DvpvklR.png",
+                "Nariman Ziyadinov",
+                "narimanz",
+                "Developer",
+                "Kazan",
+                42,
+                42
+        );
     }
 }
